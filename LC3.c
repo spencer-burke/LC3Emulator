fe05b005b342060dd45c488c81a2cb41bbf5a92e
Spencer Burke <info.spencer.burke@gmail.com>
2021-01-31 13:18:54 -0800

    Fleshing the handling of the instructions within the main method.
    Implemented:
        - the processing for the opcodes from memory
        - accepting the vm file from a command line argument
    
    On branch main
    
    Changes to be committed:
        modified:   LC3.c

--- LC3.c
+++ LC3.c
@@ -112,8 +112,88 @@ enum
  */
 int main(int argc, const char* argv[])
 {
+    if (argc < 2)
+    {
+        //show usage string
+        printf("lc3 [image-file1] ...\n");
+        exit(2);
+    }
+
+    for (int j = 1; j < argc; ++j)
+    {
+        if (!read_image(argv[j]))
+        {
+            printf("failed to load image %s\n", argv[j]);
+            exit(1);
+        }
+    }
+    // setup 12
+    /*
+     * set the PC to the starting position 
+     * 0x3000 is the default
+     * the hex value is used to iterate through the array not with indices, but with raw memory values
+     */
     enum { PC_START = 0x3000};
-    // the hex value is used to iterate through the array not with indices, but with raw memory values
     reg[R_PC] = PC_START;
+
+    int running = 1;
+    while (running) 
+    {
+        // FETCH INSTRUCTION FROM MEMORY
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+
+        switch (op)
+        {
+            case OP_ADD:
+                {ADD, /*6*/}
+                break;
+            case OP_AND:
+                {AND, /*7*/}
+                break;
+            case OP_NOT:
+                {NOT, /*7*/}
+                break;
+            case OP_BR:
+                {BR, /*7*/}
+                break;
+            case OP_JMP:
+                {JMP, /*7*/}
+                break;
+            case OP_JSR:
+                {JSR, /*7*/}
+                break;
+            case OP_LD:
+                {LD, /*7*/}
+                break;
+            case OP_LDI:
+                {LDI, /*6*/}
+                break;
+            case OP_LDR:
+                {LDR, /*7*/}
+                break;
+            case OP_LEA:
+                {LEA, /*7*/}
+                break;
+            case OP_ST:
+                {ST, /*7*/}
+                break;
+            case OP_STI:
+                {STI, /*7*/}
+                break;
+            case OP_STR:
+                {STR, /*7*/}
+                break;
+            case OP_TRAP:
+                {TRAP, /*8*/}
+                break;
+            case OP_RES:
+            case OP_RTI:
+            default:
+                {BAD_OPCODE, /*8*/} 
+                break;
+        }
+    }
+    /*{Shutdown, 12}*/
 }
 
